9cd008b129bdfcde0645f32f9108d514ef79ed0a
jaytlennon <jaytlennon@users.noreply.github.com>
2026-01-09 05:18:13 +0000

    deploy: e0fbef30e172bd251562ca897416e6eae050f9e5

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -36,7 +36,7 @@ var store = [{
         "teaser": "/assets/images/plaques.png"
       },{
         "title": "Publications",
-        "excerpt":"Karakoç C, Shoemaker WR, Lennon JT (2025) Evolutionary bioenergetics of sporulation. bioRxiv doi:10.1101/2025.08.26.672491 Mueller EA, van der Elst L, Gumennik A, Lennon JT (2025) The Enterostat: a 3D-printed bioreactor for simulating gut microbiome dynamics. bioRxiv doi:10.1101/2025.08.21.671663 Jansson JK, Flamholz AI, Peixoto RS, Salles JF, Lennon JT, Rosado A, Sanders IR,...","categories": [],
+        "excerpt":"Karakoç C, Shoemaker WR, Lennon JT (2025) Evolutionary bioenergetics of sporulation. bioRxiv doi:10.1101/2025.08.26.672491 In press at Proceedings of the National Academy of Sciences of the United States of America Mueller EA, van der Elst L, Gumennik A, Lennon JT (2025) The Enterostat: a 3D-printed bioreactor for simulating gut microbiome dynamics....","categories": [],
         "tags": [],
         "url": "/publications/",
         "teaser": "/assets/images/plaques.png"
